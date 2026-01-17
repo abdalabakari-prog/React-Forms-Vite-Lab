5676b81c48d4beca25d8d05a98c2d3e866f95c21
abdalabakari-prog <abdala.bakari@student.moringaschool.com>
2026-01-17 09:11:26 +0300

    Update Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange }) {
+  const [search, setSearch] = useState("");
+
+  // Handle search input change
+  function handleSearchChange(event) {
+    const searchValue = event.target.value;
+    setSearch(searchValue);
+    onSearchChange(searchValue);
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input 
+        type="text" 
+        name="search" 
+        placeholder="Search..." 
+        value={search}
+        onChange={handleSearchChange}
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
